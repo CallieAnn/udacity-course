645661c4834616cd118172d355457199d21435f2
CallieAnn <dedorec@my.lanecc.edu>
2019-03-22 15:27:18 -0700

    navigate to converter screen

--- course/04_navigation/task_04_navigation/lib/category.dart
+++ course/04_navigation/task_04_navigation/lib/category.dart
@@ -47,7 +47,26 @@ class Category extends StatelessWidget {
 
   /// Navigates to the [ConverterRoute].
   void _navigateToConverter(BuildContext context) {
-    // TODO: Using the Navigator, navigate to the [ConverterRoute]
+    Navigator.of(context).push(MaterialPageRoute<Null>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(
+              name,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            color: color,
+            name: name,
+            units: units,
+          ),
+        );
+      },
+    ));
   }
 
   /// Builds a custom widget that shows [Category] information.
@@ -71,7 +90,7 @@ class Category extends StatelessWidget {
           // syntax.
           // TODO: Update this onTap property to call _navigateToConverter()
           onTap: () {
-            print('I was tapped!');
+            _navigateToConverter(context);
           },
           child: Padding(
             padding: EdgeInsets.all(8.0),
